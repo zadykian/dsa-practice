b32842620fd6f77ea367c925c90467b554c2fd42
zadykian <zadykian@outlook.com>
2022-02-01 16:40:53 +0300

    RegexStateMachine

---
+++ src/DsaPractice.RegexStateMachine/src/StateMachine.fs
@@ -0,0 +1,2 @@
+module DsaPractice.RegexStateMachine.StateMachine
+
---
+++ src/DsaPractice.RegexStateMachine/test/RegexToEpsNfaTests.fs
@@ -0,0 +1,6 @@
+module ``Regex to Epsilon-NFA conversion tests``
+
+    open NUnit.Framework
+
+    [<Test>]
+    let ``implement me`` () = failwith "not implemented!"
